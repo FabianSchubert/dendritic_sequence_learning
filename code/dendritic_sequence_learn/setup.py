53a3f1fb2e931443e1feed613b653b51dec5b261
= <=>
2018-10-12 00:19:23 +0200

    .

---
+++ code/dendritic_sequence_learn/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+
+setup(	name='dendritic_sequence_learn',
+		version='0.1',
+		description='package used in the simulations and plotting routines',
+		author='Fabian Schubert',
+		packages=find_packages(),
+		install_requires=[
+          'matplotlib',
+      	],
+		zip_safe=False	)
+
+with open()
--- code/single_neuron/one_distal_mult_prox/plotting/plot_settings.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.style.use('../../../../misc/custom_style.mplstyle')
-simfold = "../../../../sim_data/single_neuron/one_distal_mult_prox/"
-plotsfold = "../../../../plots/single_neuron/one_distal_mult_prox/"
-dat_format = "pdf"
